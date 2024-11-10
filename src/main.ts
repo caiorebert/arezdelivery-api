fce0320cc0941c43e997ca98c8270d2c31e0d55c
Caio Rebert <caiorebert070802.cr@gmail.com>
2024-11-10 00:12:08 -0300

    corrigindo cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // Replace with your frontend URL
+    origin: 'https://arezdelivery-front.vercel.app/', // Replace with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
     credentials: true, // Enable cookies to be sent with requests
   });

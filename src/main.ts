88a19bea7b0e80592126009ffc8617ea6044d143
Caio Rebert <caiorebert070802.cr@gmail.com>
2024-11-21 08:55:27 -0300

    corrigindo cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     origin: '*', // Replace with your frontend URL
     allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Access-Control-Allow-Origin',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
-    credentials: true, // Enable cookies to be sent with requests
+    credentials: false, // Enable cookies to be sent with requests
   });
 
   await app.listen(process.env.PORT || 3000);

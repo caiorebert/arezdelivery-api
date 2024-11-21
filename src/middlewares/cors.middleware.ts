33eb155e515c95772744513d94bedbe11b6a7e83
Caio Rebert <caiorebert070802.cr@gmail.com>
2024-11-21 15:09:40 -0300

    logando

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,9 +1,10 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    Logger.log("entrando no middleware");
     if (req.method === 'OPTIONS') {
         console.log(1);
       res.header('Access-Control-Allow-Origin', '*'); // Altere conforme necessário

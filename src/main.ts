30ac502e55e0823a01a5f02390cda40a7eb78e53
Caio Rebert <caiorebert070802.cr@gmail.com>
2024-11-21 12:37:59 -0300

    testando implement cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['*'], // Substitua pelo domínio do frontend
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Se cookies/autenticação forem necessários
+  });
 
   await app.listen(process.env.PORT || 3000);
 }

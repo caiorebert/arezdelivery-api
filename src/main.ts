17dc9fec66be2e936209db6aa03c993cfc9dc3e7
Caio Rebert <caiorebert070802.cr@gmail.com>
2024-11-21 12:23:40 -0300

    configurando origin no env

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // Replace with your frontend URL
+    origin: process.env.FRONT_URL, // Replace with your frontend URL
     allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Access-Control-Allow-Origin',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed methods
     credentials: true, // Enable cookies to be sent with requests

f7e926151dd0dbef11d419e514b41cb0179836e1
Caio Rebert <caiorebert070802.cr@gmail.com>
2024-11-21 11:36:33 -0300

    permitindo origin

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -5,7 +5,7 @@ import { Request, Response, NextFunction } from 'express';
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Altere conforme necessário
+      res.header('Access-Control-Allow-Origin', '*'); // Altere conforme necessário
       res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       res.sendStatus(204); // No Content

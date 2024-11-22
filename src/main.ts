fbd69773b5d64e72944038603547478bac28a0bf
Caio Rebert Oliveira da Silva <77111117+caiorebert@users.noreply.github.com>
2024-11-22 11:12:00 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['*'], // Substitua pelo domínio do frontend
+    origin: '*', // Substitua pelo domínio do frontend
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Se cookies/autenticação forem necessários

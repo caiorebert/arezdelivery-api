b3be1fd605415b0f3783297c87ac22962ce2f422
Caio Rebert <caiorebert070802.cr@gmail.com>
2024-11-23 13:15:47 -0300

    teste de cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
